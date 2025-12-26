868d9fc83c5def3148534e4b8553e326b45b7d08
abdlrhmn0x7e <abdalrahman.vim@gmail.com>
2025-12-26 03:26:59 +0200

    refactor: update the tokens table skeleton to match the new layout

--- app/[locale]/admin/_components/tables/tokens/index.tsx
+++ app/[locale]/admin/_components/tables/tokens/index.tsx
@@ -1,7 +1,6 @@
 import { api } from "@/convex/_generated/api";
 import { preloadAuthQuery } from "@/lib/auth-server";
 import { TokensTableClient } from "./table";
-import { Card, CardContent } from "@/components/ui/card";
 import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import { TokensTableHeader } from "./header";
 import { Skeleton } from "@/components/ui/skeleton";
@@ -14,33 +13,27 @@ export async function TokensTable() {
 
 export function TokensTableSkeleton() {
   return (
-    <Card className="gap-1 py-2">
-      <CardContent className="px-2">
-        <div className="bg-background overflow-hidden rounded-md border">
-          <Table>
-            <TokensTableHeader />
+    <Table>
+      <TokensTableHeader />
 
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, index) => (
-                <TableRow key={index}>
-                  <TableCell>
-                    <Skeleton className="h-4 w-full" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton className="h-4 w-full" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton className="h-4 w-full" />
-                  </TableCell>
-                  <TableCell>
-                    <Skeleton className="h-4 w-full" />
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </div>
-      </CardContent>
-    </Card>
+      <TableBody>
+        {Array.from({ length: 10 }).map((_, index) => (
+          <TableRow key={index}>
+            <TableCell>
+              <Skeleton className="h-4 w-full" />
+            </TableCell>
+            <TableCell>
+              <Skeleton className="h-4 w-full" />
+            </TableCell>
+            <TableCell>
+              <Skeleton className="h-4 w-full" />
+            </TableCell>
+            <TableCell>
+              <Skeleton className="h-4 w-full" />
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   );
 }
