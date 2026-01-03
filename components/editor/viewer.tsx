f0a752c4b2cc7b560dd7d3b2d7ac7fcaaffa6e0e
Mahmoud Omiesh <mahmoudomiesh2005@gmail.com>
2026-01-03 23:05:23 +0200

    fix: tiptap viewer content wasn't changing after mount

--- components/editor/viewer.tsx
+++ components/editor/viewer.tsx
@@ -2,9 +2,16 @@
 
 import { EditorContent, type JSONContent } from "@tiptap/react";
 import { useEditor } from "@/components/editor/use-editor";
+import { useEffect } from "react";
 
 export function TiptapViewer({ content }: { content: JSONContent }) {
   const { editor } = useEditor({ defaultContent: content });
+
+  useEffect(() => {
+    if (editor) {
+      editor.commands.setContent(content);
+    }
+  }, [editor, content]);
 
   if (!editor) return null;
 
